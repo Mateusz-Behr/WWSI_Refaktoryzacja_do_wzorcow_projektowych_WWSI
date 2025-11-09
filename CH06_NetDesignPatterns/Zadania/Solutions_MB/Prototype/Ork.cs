087164756da42e7861c1d8a4e23dafe8d6e0341a
Mateusz Behr <behrmat@gmail.com>
2025-11-09 17:51:57 +0100

    Dodano zadanie Prototype

---
+++ CH06_NetDesignPatterns/Zadania/Solutions_MB/Prototype/Ork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringToDesignPatterns.CH06_NetDesignPatterns.Zadania.Solutions_MB.Prototype
+{
+    public class Ork
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Health { get; set; }
+        public string Weapon { get; set; }
+        public string SkinColor { get; set; }
+        public string Tribe { get; set; }
+
+ 
+        public Ork(string name, int strength, int health, string weapon, string skinColor, string tribe)
+        {
+            Name = name;
+            Strength = strength;
+            Health = health;
+            Weapon = weapon;
+            SkinColor = skinColor;
+            Tribe = tribe;
+        }
+
+
+        public void DisplayOrkInfo()
+        {
+            Console.WriteLine($"Ork: {Name}");
+            Console.WriteLine($"  Siła: {Strength}, Zdrowie: {Health}");
+            Console.WriteLine($"  Broń: {Weapon}, Kolor skóry: {SkinColor}, Plemię: {Tribe}");
+        }
+    }
+}
---
+++ CH06_NetDesignPatterns/Zadania/Solutions_MB/Prototype/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringToDesignPatterns.CH06_NetDesignPatterns.Zadania.Solutions_MB.Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Oryginalny Ork (Prototyp) ---");
+            Ork originalOrk = new Ork(
+                "Garrosh",
+                80,    // Początkowa siła
+                150,   // Początkowe zdrowie
+                "Topór",
+                "Pomarańczowy",
+                "Klan Wojennej Pieśni"
+            );
+            originalOrk.DisplayOrkInfo();
+
+            List<Ork> orkClones = new List<Ork>();
+            Random random = new Random(); // Obiekt do generowania liczb losowych
+            int numberOfClones = 5;
+
+            Console.WriteLine($"\n--- Tworzenie {numberOfClones} klonów Orka ---");
+
+            for (int i = 0; i < numberOfClones; i++)
+            {
+                string jsonString = JsonConvert.SerializeObject(originalOrk);
+                Ork clonedOrk = JsonConvert.DeserializeObject<Ork>(jsonString);
+
+                clonedOrk.Name = $"{originalOrk.Name} Klon {i + 1}";
+
+                // Zmiana siły o +/- 10 pkt
+                clonedOrk.Strength += random.Next(-10, 11);
+
+                // Zmiana zdrowia o +/- 20 punktów (np.)
+                clonedOrk.Health += random.Next(-20, 21);
+
+                orkClones.Add(clonedOrk);
+
+                Console.WriteLine($"Klon Orka {i + 1} stworzony i zmodyfikowany.");
+            }
+
+            Console.WriteLine("\n--- Lista Sklonowanych Orków ---");
+            foreach (var ork in orkClones)
+            {
+                ork.DisplayOrkInfo();
+            }
+
+            Console.WriteLine("\n--- Weryfikacja: Oryginalny Ork pozostał niezmieniony ---");
+            originalOrk.DisplayOrkInfo();
+        }
+    }
+}
