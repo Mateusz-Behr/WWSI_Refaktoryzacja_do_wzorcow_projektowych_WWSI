f569840c6d8458ffcdc14562b17c798e188b7894
Mateusz Behr <behrmat@gmail.com>
2025-11-16 00:03:56 +0100

    Dodano zadanie Proxy

---
+++ CH06_NetDesignPatterns/Zadania/Solutions_MB/Proxy/IFileAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringToDesignPatterns.CH06_NetDesignPatterns.Zadania.Solutions_MB.Proxy
+{
+    public interface IFileAccess
+    {
+        void ReadFile();
+        void WriteFile(string content);
+        void AppendToFile(string content);
+    }
+}
